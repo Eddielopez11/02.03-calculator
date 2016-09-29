296272e58b69a5bef1152841441a9a9486c67b90
Eduardo Lopez <eddielopez2101@gmail.com>
2016-09-29 15:26:45 -0500

    removed unnecesary margin and changed event listener to each div or child instead of parent container

--- javascript/main.js
+++ javascript/main.js
@@ -1,17 +1,18 @@
 document.addEventListener("DOMContentLoaded", function() {
 
-  var calculatorEl = document.querySelector("[data-js='calculator']");
+  var calculatorEl = document.querySelectorAll("[data-js='calculatorButton']");
   var resultEl = document.querySelector("[data-js='result']");
 
-
-  calculatorEl.addEventListener("click", function(e){
-    var targetClicked = e.target.textContent
-    if(targetClicked === "C"){
-      resultEl.innerHTML = "";
-    } else if(targetClicked === "=") {
-      resultEl.innerHTML = eval(resultEl.textContent)
-    } else {
-      resultEl.innerHTML += targetClicked;
-    }
-  });
+  calculatorEl.forEach(function(button){
+    button.addEventListener("click", function(e){
+      var targetClicked = e.target.textContent
+      if(targetClicked === "C"){
+        resultEl.innerHTML = "";
+      } else if(targetClicked === "=") {
+        resultEl.innerHTML = eval(resultEl.textContent)
+      } else {
+        resultEl.innerHTML += targetClicked;
+      }
+    });
+  })
 });
